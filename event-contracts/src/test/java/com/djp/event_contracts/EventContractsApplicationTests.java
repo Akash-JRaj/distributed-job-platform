0f9d63f279c6e38b87a4e994e7de72b462c5a8c1
Akash Jayaraj <akashjayarajvkp2k02@gmail.com>
2026-03-14 20:10:45 +0530

    Initial structure.

---
+++ event-contracts/src/test/java/com/djp/event_contracts/EventContractsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djp.event_contracts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventContractsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
